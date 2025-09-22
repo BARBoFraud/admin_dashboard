45c05b13c988eab934bc5b50c621d3a01aba56b1
Leonardo Pérez <leote@Leonardos-MacBook-Air.local>
2025-09-22 16:04:19 -0600

    feat: Proteger rutas, configurar ruteo inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./components/ProtectedRoute"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 function App() {
   return (
-    <h1>Hola mundote</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
