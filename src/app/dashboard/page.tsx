c6c5d6ebb2b3182c54ca05a7119d97a975b518e3
Leonardo Pérez <leote@Leonardos-MacBook-Air.local>
2025-10-03 16:55:27 -0600

    FUncionalidad de autenticacion

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,36 @@
-'use client';
+"use client";
 
 import { useAdminLogin } from "@/hooks/auth.hooks";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function DashboardPage() {
-    const { logout, isLoggedIn } = useAdminLogin();
-    const router = useRouter();
+  const { logout, isLoggedIn } = useAdminLogin();
+  const router = useRouter();
 
-    useEffect(() => {
-        if (!isLoggedIn) {
-            router.push('/');
-        }
-    }, [isLoggedIn, router]);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/");
+    }
+  }, [isLoggedIn, router]);
 
-    const handleLogout = () => {
-        logout();
-        router.push('/');
-    };
+  const handleLogout = () => {
+    logout();
+    router.push("/");
+  };
 
-    return (
-        <div className="p-8">
-            <div className="flex justify-between items-center mb-8">
-                <h1 className="text-3xl font-bold">Dashboard</h1>
-                <Button onClick={handleLogout} variant="default">
-                    Logout
-                </Button>
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="p-8">
+      <div className="flex justify-between items-center mb-8">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <ModeToggle />
+        <Button onClick={handleLogout} variant="default">
+          Logout
+        </Button>
+      </div>
+    </div>
+  );
 }
+
