4a4d2376428af03b73203d2e553f53221e5d6035
BarbieMunoz <a01659348@tec.mx>
2025-10-05 23:49:25 -0600

    cambio de distribución del dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,20 @@
 import { DashboardHeader } from "@/components/dashboard-header";
 import { AdminList } from "@/components/admin-list";
+import RevisionReportes from "@/components/revision-reportes";
 
 export default function DashboardPage() {
   return (
     <div className="min-h-screen bg-background">
       <DashboardHeader />
-      <main className="p-8 max-w-7xl mx-auto">
-        <div className="bg-card rounded-md p-6">
-          <div className="grid gap-6">
-            <AdminList />
+      <main className="p-5 max-w-7xl mx-auto">
+        <div className="bg-background rounded-md p-6">
+          <div className="grid grid-cols-1 md:grid-cols-[70%_30%] gap-9 items-start">
+            <div className="h-full">
+              <RevisionReportes />
+            </div>
+            <div className="h-full">
+              <AdminList />
+            </div>
           </div>
         </div>
       </main>
