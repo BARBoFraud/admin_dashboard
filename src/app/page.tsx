c4b4e92da6d7fd3ac2114e4e1609c5d04c4324d5
BarbieMunoz <a01659348@tec.mx>
2025-10-05 21:02:05 -0600

    imagen de logo y cambio  de  estilo en page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { ModeToggle } from "@/components/mode-toggle";
 import { LoginForm } from "@/components/login-form";
 import { useAdminLogin } from "@/hooks/auth.hooks";
 import { useRouter } from "next/navigation";
@@ -19,7 +17,6 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center text-foreground min-h-screen justify-center">
       <div className="absolute top-4 right-4">
-        <ModeToggle />
       </div>
       <div className="w-full max-w-md">
         <LoginForm />
