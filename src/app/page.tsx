eaa90bf696bbb9f985b32e770d528c3464914f06
Leonardo Pérez <leote@Leonardos-MacBook-Air.local>
2025-10-06 15:50:13 -0600

    correccion de mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,7 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center text-foreground min-h-screen justify-center">
-      <div className="absolute top-4 right-4">
-      </div>
+      <div className="absolute top-4 right-4"></div>
       <div className="w-full max-w-md">
         <LoginForm />
       </div>
