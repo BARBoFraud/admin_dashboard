49f02ca78755e62200b9dba220ad56f36e6b6339
BarbieMunoz <a01659348@tec.mx>
2025-10-05 20:55:35 -0600

    mejore estilo y agregue menu de hamburguesa

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -3,12 +3,16 @@
 import { useAdminLogin } from "@/hooks/auth.hooks";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ModeToggle } from "@/components/mode-toggle";
+import logo from "@/components/assets/imgMaluma.jpeg";
+import { Menu } from "lucide-react";
+import { SideMenu } from "@/components/side-menu";
 
 export function DashboardHeader() {
-  const { logout, isLoggedIn } = useAdminLogin();
+  const {isLoggedIn } = useAdminLogin();
   const router = useRouter();
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -16,20 +20,21 @@ export function DashboardHeader() {
     }
   }, [isLoggedIn, router]);
 
-  const handleLogout = () => {
-    logout();
-    router.push("/");
-  };
-
   return (
-    <div className="flex justify-between items-center mb-8">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
-      <div className="flex items-center gap-4">
-        <ModeToggle />
-        <Button onClick={handleLogout} variant="default">
-          Logout
-        </Button>
+    <header className="w-full bg-sidebar text-sidebar-foreground shadow-lg border-b border-sidebar-border">
+      <div className="max-w-7xl mx-auto px-0 py-10 flex justify-between items-center">
+        <div className="flex items-center gap-4">
+          <Button variant="outline" size="icon" onClick={() => setOpen(true)}>
+            <Menu className="h-6 w-6" />
+          </Button>
+          <img src={logo.src} alt="Logo" className="w-12 h-12 rounded-full" />
+          <h1 className="text-5xl font-bold">oFraud</h1>
+        </div>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+        </div>
       </div>
-    </div>
+      <SideMenu open={open} onClose={() => setOpen(false)} />
+    </header>
   );
 }
