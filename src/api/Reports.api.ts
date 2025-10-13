41b6894bbe4d7b7b0801efc48d7142c633044bf4
BarbieMunoz <a01659348@tec.mx>
2025-10-12 23:47:19 -0600

    funcion para tener los reportes rechazados y aceptados

--- src/api/Reports.api.ts
+++ src/api/Reports.api.ts
@@ -35,5 +35,53 @@ export function useReportsApi() {
     }
   }, [accessToken, refreshTokenFunc]);
 
-  return { getPendingReports };
+  const getAcceptedReports = useCallback(async () => {
+    try {
+      const response = await axios.get<ShortPendingReport[]>(
+        `${BASE_URL}/v1/reports/dashboard/accepted`,
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        await refreshTokenFunc();
+        const newAccessToken = localStorage.getItem("accessToken");
+        if (!newAccessToken) {
+          throw new Error("No se pudo refrescar el token");
+        }
+        const retryResponse = await axios.get<ShortPendingReport[]>(
+          `${BASE_URL}/v1/reports/dashboard/feed`,
+          { headers: { Authorization: `Bearer ${newAccessToken}` } }
+        );
+        return retryResponse.data;
+      }
+      throw error;
+    }
+  }, [accessToken, refreshTokenFunc]);
+
+  const getRejectedReports = useCallback(async () => {
+    try {
+      const response = await axios.get<ShortPendingReport[]>(
+        `${BASE_URL}/v1/reports/dashboard/rejected`,
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        await refreshTokenFunc();
+        const newAccessToken = localStorage.getItem("accessToken");
+        if (!newAccessToken) {
+          throw new Error("No se pudo refrescar el token");
+        }
+        const retryResponse = await axios.get<ShortPendingReport[]>(
+          `${BASE_URL}/v1/reports/dashboard/rejected`,
+          { headers: { Authorization: `Bearer ${newAccessToken}` } }
+        );
+        return retryResponse.data;
+      }
+      throw error;
+    }
+  }, [accessToken, refreshTokenFunc]);
+
+  return { getPendingReports, getAcceptedReports, getRejectedReports };
 }
