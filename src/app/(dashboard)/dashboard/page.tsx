17fd7595b55176e98fc531355fdb95cd1c3ef56b
BarbieMunoz <a01659348@tec.mx>
2025-10-12 21:45:26 -0600

    cambio de estilo

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,9 +4,15 @@ import ReportsList from "@/components/features/ReportsList";
 
 export default function DashboardPage() {
   return (
-    <div>
-      <ReportsList />
-      <CategoriesGraph />
+    <div className="w-full p-15">
+      <div className="grid gap-6 md:grid-cols-[75%_25%] items-start">
+        <div className="w-full">
+          <ReportsList />
+        </div>
+        <div className="w-full">
+          <CategoriesGraph />
+        </div>
+      </div>
     </div>
   );
 }
