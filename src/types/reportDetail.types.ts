8ff1d153928583b2344ac1dc66dc3f8ee35fc372
BarbieMunoz <a01659348@tec.mx>
2025-10-12 21:48:25 -0600

    dtos de reporte y status

---
+++ src/types/reportDetail.types.ts
@@ -0,0 +1,15 @@
+export type DetailedReport = {
+  id: number;
+  name?: string;
+  lastName?: string;
+  description?: string;
+  url?: string;
+  website?: string;
+  socialMedia?: string | null;
+  phoneNumber?: string | null;
+  createdAt: string;
+  username?: string | null;
+  email?: string | null;
+  image?: string | null;
+  category: string;
+};
